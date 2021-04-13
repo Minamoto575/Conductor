83f54c8663532f22868d87a553ae67cab7fa3579
krl <911190218@qq.com>
2021-04-13 15:56:59 +0800

    log in

--- pages/auth/auth.js
+++ pages/auth/auth.js
@@ -4,13 +4,41 @@ Page({
     StatusBar: app.globalData.StatusBar,
     CustomBar: app.globalData.CustomBar,
     ColorList: app.globalData.ColorList,
-  },
-  onGetUserInfo: function (e) {
-    if (!this.logged && e.detail.userInfo) {
-      app.globalData.userInfo = e.detail.userInfo;
-      wx.switchTab({
-        url: '/pages/home/index',
-      })
+    user:{
+      uid: '',
+      username:'',
+      phone:'',
+      age:'',
+      gender:'',
+      address:'',
+      role:1,
+      token:''
     }
-  }
+  },
+  formSubmit(e) {
+    var phone = e.detail.value.phone;
+    console.log(phone);
+    wx.switchTab({
+      url: '/pages/home/index/index',
+    })
+    // if (!this.logged && e.detail.userInfo) {
+    //   app.globalData.userInfo = e.detail.userInfo;
+    //   wx.switchTab({
+    //     url: '/pages/home/index',
+    //   })
+    // }
+    // wx.request({
+    //   url: 'http://xx.com/user/login?phone'+phone,
+    //   success(res){
+    //     var user = res.data;
+    //     this.setData({
+    //       user:user
+    //     })
+    //     wx.switchTab({
+    //         url: '/pages/home/index',
+    //     })
+    //   }
+      
+    // })    
+  },
 });
